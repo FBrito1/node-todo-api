39d4d1051329d9ff6e90ae4bbfc17f8315dfd293
Fabio Brito <luan.tecno@gmail.com>
2017-11-24 11:40:08 -0200

    Setup addons for heroku

--- server/db/moongose.js
+++ server/db/moongose.js
@@ -1,8 +1,8 @@
 var mongoose = require('mongoose');
-var { MONGODB_URI } = require('./../../mongodbconnection/mongo');
+
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect(MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = { mongoose };
